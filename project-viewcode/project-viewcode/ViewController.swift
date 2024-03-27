b5e4ebb95df0f67877e601a7e2cb93dbf9c666eb
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-03-27 08:03:36 -0300

    remove storyboard

---
+++ project-viewcode/project-viewcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-viewcode
+//
+//  Created by Kaique Lopes de Oliveira on 27/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
